9b1cc13edf3f3e7b407d881cead6a9076a380c62
Minji <kmjj1208@gmail.com>
2023-02-14 22:02:33 +0900

    day47, 백준 2460

--- Java/JavaCoding/study/src/baekjoon/bronze/P_2460.java
+++ Java/JavaCoding/study/src/baekjoon/bronze/P_2460.java
@@ -1,10 +1,25 @@
 package baekjoon.bronze;
 
+import java.util.Scanner;
+
 public class P_2460 {
 
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
+		int max_num=0;
+		int sum=0;
+		int in_num, out_num;
+		
+		do {
+			out_num = sc.nextInt();
+			in_num = sc.nextInt();
+			sum = sum+in_num-out_num;
+			if(sum>max_num) {
+				max_num=sum;				
+			}
+		} while(sum!=0);
+		System.out.println(max_num);
 	}
 
 }
--- day45.md
+++ day45.md
@@ -4,3 +4,9 @@
 ## 2023/02/13
 영속성 프레임워크 : 정보에 대한 접근과 저장을 단순화하는 라이브러리  
 Mybatis를 이용하면 자동으로 Connection, close()가 가능함.
+
+## 2023/02/14(🍫day)
+CDATA Section은 XML 고유의 문법, CDATA 영역에 작성된 데이터는 단순한 문자 데이터(Character DATA)이므로 XML 파서가 해석하지 않도록 함.  
+Mybatis에서 if라는 조건이 만족되면 where을 사용하고 if라는 조건이 만족도ㅣ지 않으면 where 생략하기  
+resultType : resultSet으로부터 얻은 값을 어떤 타입으로 반환해줄 것인지 설정하는 속성  
+update, delete, insert는 결과값을 자동으로 반환해주기 때문에 resultTye 사용 불가능
